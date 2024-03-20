4f1cd1e3d1ddaf78d0ea2ccdc4f34fc0427ca469
utcl2633 <utcl2633@gmail.com>
2024-03-20 11:37:59 +0530

    onclick profile navigate to login page

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
     selector: 'app-dashboard',
     standalone: true,
     templateUrl: './dashboard.component.html',
     styleUrl: './dashboard.component.css',
-    imports: [HeaderComponent]
+    imports: [HeaderComponent, MatButtonModule, MatMenuModule,MatIconModule,RouterLink],
 })
 export class DashboardComponent {
 
