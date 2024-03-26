b3ed63d0c8e4e9f1e0c25718fbdfb437f41502f9
utcl2633 <utcl2633@gmail.com>
2024-03-26 10:21:01 +0530

    logout button

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
 import {MatMenuModule} from '@angular/material/menu';
 import {MatButtonModule} from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { LocalStorageService } from '../../services/local-storage.service';
 
 @Component({
     selector: 'app-dashboard',
@@ -13,5 +14,13 @@ import { RouterLink } from '@angular/router';
     imports: [HeaderComponent, MatButtonModule, MatMenuModule,MatIconModule,RouterLink],
 })
 export class DashboardComponent {
+  router = inject(Router);
 
+    constructor(private localStorageService: LocalStorageService) {
+    }
+    logout() {    
+        this.localStorageService.removeItem('loggedInUser');
+        this.router.navigate(['/login']);
+        
+      }
 }
